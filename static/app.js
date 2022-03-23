72f4e3995bd7500e4125148f2ef8ead3744e7cca
norojordan <95200975+norojordan@users.noreply.github.com>
2022-03-23 18:15:25 -0400

    CORS error idkk

--- static/app.js
+++ static/app.js
@@ -1,7 +1,11 @@
-// Get the Roadster endpoint
-const navels = "https://robdunnlab.com/projects/belly-button-biodiversity/";
+
 
 // Fetch the JSON data and console log it
-d3.json(navels).then(function(samples) {
+d3.json("samples.json").then((data) => {
+  let names = data.names;
+  console.log(names);
+  let metadata = data.metadata;
+  console.log(metadata);
+  let samples = data.samples;
   console.log(samples);
 });
