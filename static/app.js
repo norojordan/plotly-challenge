20f9e8c40ab5343575960254fb953241fcd40a65
norojordan <95200975+norojordan@users.noreply.github.com>
2022-03-25 01:15:42 -0400

    working on plots

--- static/app.js
+++ static/app.js
@@ -1,50 +1,47 @@
 // CORS error when trying to import, so use url on github - it worked!
-const url = "https://raw.githubusercontent.com/norojordan/plotly-challenge/main/samples.json";
-
-// Fetch the JSON data and console log it
-d3.json(url).then((data) => {
-  console.log(data);
-});
-
-// Sort the data by id numbers
-//let sortedsamples = data.sort((a, b) => b.id - a.id);
-//console.log(sortedsamples)
-
-// Filter sample values by selected id
-let filteredSample = data.filter(samples => samples.id === id)[0];
-console.log(filteredSample);
-
-// Slice the first 10 objects for plotting
-let values = filteredSample.sample_values.slice(0, 10);
-
-// Reverse the array to accommodate Plotly's defaults
-reversedvalues = values.reverse();
+//const url = "https://raw.githubusercontent.com/norojordan/plotly-challenge/main/samples.json";
+//d3.json(url).then((data) =>{
+  //console.log(data)
 
 
-
-// Trace 1 for the top 10 otus in an individual
-let trace1 = {
-  x: reversedvalues.map(object => object.id),
-  y: reversedvalues.map(object => object.otuids),
-  text: reversedvalues.map(object => object.otuids),
-  type: "bar",
-  orientation: "h",
-  hovertext: "otulabels"
-
-};
-
-// Data trace array
-let traceData = [trace1];
-
-// Apply the group barmode to the layout
-let layout = {
-title: "Top 10 OTU's for Individuals"
-};
+//Create plots
+function createplots(id) {
+  // Fetch the JSON data and console log it. Make sure to start sever first!
+  d3.json("samples.json").then((data) => {
+    console.log(data)
 
 
+  // Filter sample values by selected id
+    let samples = data.samples;
+    console.log(samples);
+    let filteredsample = samples.filter(sample => sample.id === id)[0];
+    console.log(filteredsample);
 
+  // Slice the first 10 objects for plotting & reverse for Plotly
+    let values = filteredsample.sample_values.slice(0, 10).reverse();
+    let otuids = filteredsample.otu_ids.map(otuid => `OTU ${otuid}`).slice(0, 10).reverse();
+    let otulabels = filteredsample.otu_labels.slice(0, 10).reverse();
+ 
+  // Trace 1 for the top 10 otus in an individual
+    let trace1 = {
+      x: values,
+      y: otuids,
+      text: otuidslabels,
+      type: "bar",
+      orientation: "h",
+      hovertext: "otulabels"
+    };  
 
+    // Data trace array
+    let barData1 = [trace1];
 
+    // Apply a title to the layout
+    let layout1 = {
+    title: "Top 10 OTU's for Individuals"
+    };
 
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", traceData, layout);
+    // Render the plot to the div tag with id "bar"
+    Plotly.newPlot("bar", barData1, layout1);
+    console.log(barData1)
+    })
+}; 
