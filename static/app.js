68fb1a3278b43e433aa69cb7fa182dc4133a0ad9
norojordan <95200975+norojordan@users.noreply.github.com>
2022-03-26 17:25:31 -0400

    got charts

--- static/app.js
+++ static/app.js
@@ -1,8 +1,9 @@
 // CORS error when trying to import, so use url on github - it worked!
-const url = "https://raw.githubusercontent.com/norojordan/plotly-challenge/main/samples.json";
-d3.json(url).then((data) =>{
+const url =
+  "https://raw.githubusercontent.com/norojordan/plotly-challenge/main/samples.json";
+d3.json(url).then((data) => {
   console.log(data);
-});
+})
 // Promise Pending
 //const data = d3.json(url);
 //console.log("Data Promise: ", data);
@@ -11,55 +12,52 @@ d3.json(url).then((data) =>{
 
 //console.log("After the promise consumption")
 
-
-
 //Create plots
 //function createPlots(id) {
-  // Fetch the JSON data and console log it. Make sure to start sever first!
-  //d3.json(url).then((data) =>{
-    //console.log(data)
-  
-    // Filter sample values in samples array by selected id
+// Fetch the JSON data and console log it. Make sure to start sever first!
+//d3.json(url).then((data) =>{
+//console.log(data)
 
-    //let filteredSample = data.samples.filter(sample => sample.id === id)[0];
-   // console.log("Filtered Sample:",filteredSample);
-   
+// Filter sample values in samples array by selected id
 
-    // Slice the first 10 objects for plotting & reverse for Plotly
-    //let values = filteredSample.sample_values.slice(0, 10).reverse();
-    //let otuids = filteredSample.otu_ids.map(otuid => `OTU ${otuid}`).slice(0, 10).reverse();
-    //let otulabels = filteredSample.otu_labels.slice(0, 10).reverse();
- 
-   //Trace 1 for the top 10 otus in an individual
-    //let trace1 = {
-      //x: values,
-      //y: otuids,
-      //text: otuidslabels,
-      //type: "bar",
-      //orientation: "h",
-      //hovertext: "otulabels"
-    //};  
+//let filteredSample = data.samples.filter(sample => sample.id === id)[0];
+// console.log("Filtered Sample:",filteredSample);
 
-    // Data trace array
-    //let barData1 = [trace1];
+// Slice the first 10 objects for plotting & reverse for Plotly
+//let values = filteredSample.sample_values.slice(0, 10).reverse();
+//let otuids = filteredSample.otu_ids.map(otuid => `OTU ${otuid}`).slice(0, 10).reverse();
+//let otulabels = filteredSample.otu_labels.slice(0, 10).reverse();
 
-    // Apply a title to the layout
-    //let layout1 = {
-    //title: "Top 10 OTU's for Individuals"
-    //};
+//Trace 1 for the top 10 otus in an individual
+//let trace1 = {
+//x: values,
+//y: otuids,
+//text: otuidslabels,
+//type: "bar",
+//orientation: "h",
+//hovertext: "otulabels"
+//};
 
-    // Render the plot to the div tag with id "bar"
-    //Plotly.newPlot("bar", barData1, layout1);
-  //});
+// Data trace array
+//let barData1 = [trace1];
+
+// Apply a title to the layout
+//let layout1 = {
+//title: "Top 10 OTU's for Individuals"
+//};
+
+// Render the plot to the div tag with id "bar"
+//Plotly.newPlot("bar", barData1, layout1);
+//});
 //}
 
-function buildMetadataDisplay(sample){
-  d3.json(url).then(function(data){
+function buildMetadataDisplay(sample) {
+  d3.json(url).then(function (data) {
     let metadata = data.metadata;
 
-    let sampleMetaData = metadata.filter(sampleObj => sampleObj.id == sample);
+    let sampleMetaData = metadata.filter((sampleOBJ) => sampleOBJ.id == sample);
 
-    if (sampleMetaData.length >0){
+    if (sampleMetaData.length > 0) {
       let resultSample = sampleMetaData[0];
       let metaDataPanel = d3.select("#sample-metadata");
 
@@ -68,100 +66,108 @@ function buildMetadataDisplay(sample){
 
       //const test = Object.entries(resultSample);
 
-      Object.entries(resultSample).forEach(([key,value]) => {
+      Object.entries(resultSample).forEach(([key, value]) => {
         metaDataPanel.append("h6").text(`${key}: ${value}`);
       });
     } else {
       console.log("data error");
-    } 
-  });
-}     
-        
+    }
+  })
+}
+
 //console.log(`Meta: ${sample}`);
 
-
-function buildCharts(sample){
-
-  d3.json(url).then(function(data){
-    let samples= data.samples;
-    let filteredSample = samples.filter(sample => sample.id == id);
+function buildCharts(sample) {
+  d3.json(url).then(function (data) {
+    let samples = data.samples;
+    let filteredSample = samples.filter((sample) => sample.id == id);
     //let sampleData = samples.filter((sampleObj) => sampleObj.id ==sample);
-    
-    if (filteredSample.length > 0){
 
-       // Slice the first 10 objects for plotting & reverse for Plotly
-       let result = filteredSample[0];
-       let values = result.sample_values.slice(0, 10).reverse();
-       let otuids = result.otu_ids.map(otuid => `OTU ${otuid}`).slice(0, 10).reverse();
-       let otulabels = result.otu_labels.slice(0, 10).reverse();
-       //let result = sampleData[0];
+    if (filteredSample.length > 0) {
+      // Slice the first 10 objects for plotting & reverse for Plotly
+      let result = filteredSample[0];
+      let values = result.sample_values.slice(0, 10).reverse();
+      let otuids = result.otu_ids
+        .map((otuid) => `OTU ${otuid}`)
+        .slice(0, 10)
+        .reverse();
+      let otulabels = result.otu_labels.slice(0, 10).reverse();
+      //let result = sampleData[0];
 
-       //let otu_ids = result.otu_ids;
-       //let otu_labels = result.otu_labels;
-       //let sample_values = result.sample_values;
+      //let otu_ids = result.otu_ids;
+      //let otu_labels = result.otu_labels;
+      //let sample_values = result.sample_values;
 
-       let bubbleData2 =[{
-         x:  otuids,
+       let trace2 = {
+         x: otuids,
          y: values,
-         text: otulabels
-       }]
+         text: otulabels,
+         mode: "markers",
+         marker: {
+             size: values,
+             color: otuids
+           },
+       };
+       let bubbleData2 = [trace2];
 
+       let layout2 = {
+         title: "Bubble Chart for OTU ID",
+       };
+
+       // Render the plot to the div tag with id "bar"
+       Plotly.newPlot("bubble", bubbleData2, layout2);
 
       //Trace 1 for the top 10 otus in an individual
-       let trace1 = {
+      let trace1 = {
         x: values,
         y: otuids,
         text: otulabels,
         type: "bar",
         orientation: "h",
-        hovertext: "otu_labels"
-    };  
-
-    // Data trace array
-      let barData1 = [trace1];
-
-    // Apply a title to the layout
-      let layout1 = {
-      title: "Top 10 OTU's for Individuals"
+        hovertext: "otu_labels",
       };
 
-    // Render the plot to the div tag with id "bar"
-      Plotly.newPlot("bar", barData1, layout1);
-    };
-    
-  });
-  //console.log(`Build: ${sample}`);
+      // Data trace array
+      let barData1 = [trace1];
 
+      // Apply a title to the layout
+      let layout1 = {
+        title: "Top 10 OTU's for Individuals",
+      };
+
+      // Render the plot to the div tag with id "bar"
+      Plotly.newPlot("bar", barData1, layout1);
+    }
+  })  
 }
 
+  //console.log(`Build: ${sample}`);
 
-function init(){
+
+function init() {
   let dropdown = d3.select("#selDataset");
-  
-  d3.json(url).then(function(data){
+
+  d3.json(url).then(function (data) {
     console.log(data);
 
     let ids = data.names;
     //console.log(names);
 
-    for (id of ids){
-      dropdown.append("option")
-        .text(id)
-        .property("value", id);
+    for (id of ids) {
+      dropdown.append("option").text(id).property("value", id);
     }
     let firstSample = ids[0];
     optionChanged(firstSample);
-    
-  });
+  })
 
-  
   //buildMetadataDisplay(0);
 
   //buildCharts(0);
 }
 
-function optionChanged(sample){
+function optionChanged(sample) {
   buildCharts(sample);
   buildMetadataDisplay(sample);
 }
+
 init();
