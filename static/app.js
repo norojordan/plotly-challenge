9ce958c6625c3769c60fc3d35980415011e544e4
norojordan <95200975+norojordan@users.noreply.github.com>
2022-03-26 00:36:36 -0400

    making little progress

--- static/app.js
+++ static/app.js
@@ -1,28 +1,36 @@
 // CORS error when trying to import, so use url on github - it worked!
-//const url = "https://raw.githubusercontent.com/norojordan/plotly-challenge/main/samples.json";
-//d3.json(url).then((data) =>{
-  //console.log(data)
+const url = "https://raw.githubusercontent.com/norojordan/plotly-challenge/main/samples.json";
+d3.json(url).then((data) =>{
+  console.log(data);
+});
+// Promise Pending
+//const data = d3.json(url);
+//console.log("Data Promise: ", data);
+
+// Fetch the JSON data and console log it
+
+//console.log("After the promise consumption")
+
 
 
 //Create plots
-function createplots(id) {
+function createPlots(id) {
   // Fetch the JSON data and console log it. Make sure to start sever first!
-  d3.json("samples.json").then((data) => {
+  d3.json(url).then((data) =>{
     console.log(data)
+  
+  // Filter sample values in samples array by selected id
 
-
-  // Filter sample values by selected id
-    let samples = data.samples;
-    console.log(samples);
-    let filteredsample = samples.filter(sample => sample.id === id)[0];
-    console.log(filteredsample);
+     let filteredSample = data.samples.filter(sample => sample.id === id)[0];
+     console.log("Filtered Sample:",filteredSample);
+   
 
   // Slice the first 10 objects for plotting & reverse for Plotly
-    let values = filteredsample.sample_values.slice(0, 10).reverse();
-    let otuids = filteredsample.otu_ids.map(otuid => `OTU ${otuid}`).slice(0, 10).reverse();
-    let otulabels = filteredsample.otu_labels.slice(0, 10).reverse();
+    let values = filteredSample.sample_values.slice(0, 10).reverse();
+    let otuids = filteredSample.otu_ids.map(otuid => `OTU ${otuid}`).slice(0, 10).reverse();
+    //let otulabels = filteredSample.otu_labels.slice(0, 10).reverse();
  
-  // Trace 1 for the top 10 otus in an individual
+   //Trace 1 for the top 10 otus in an individual
     let trace1 = {
       x: values,
       y: otuids,
@@ -42,6 +50,5 @@ function createplots(id) {
 
     // Render the plot to the div tag with id "bar"
     Plotly.newPlot("bar", barData1, layout1);
-    console.log(barData1)
-    })
-}; 
+  });
+}
